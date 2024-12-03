3fd8ca6fa745bb430f0fe3ed5540ffdac1433c6a
Palak Gontiya <113601878+Palakgontiya@users.noreply.github.com>
2024-12-03 22:07:03 +0530

    header and hero section

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -2,7 +2,27 @@ import './Header.scss'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header className='d-flex'>
+      <img className='logo' src="logo.png" alt="logo" width="172" height="54"/>
+      <div className='action-container'>
+        <ul>
+          <li>Home</li>
+          <li>Our Portfolio</li>
+          <li>contact</li>
+        </ul>
+          <ul>
+          <li className='call'> 
+            <img src="call.svg" alt="call" width="24" height="24" />
+            </li>
+          <li className='sms'>
+            <img src="sms.svg" alt="sms"  width="24" height="24" />
+          </li>
+          <li className='quote-btn'>
+            <button>Get Quote</button>
+          </li>
+          </ul>
+      </div>
+      </header>
   )
 }
 
--- src/pages/home/hero/Hero.tsx
+++ src/pages/home/hero/Hero.tsx
@@ -2,6 +2,33 @@ import './Hero.scss'
 
 export const Hero = () => {
   return (
-    <div>Hero</div>
+    <div className="hero-container">
+      <div className='left-side'>
+      <h1>We Build Best, Advance<br/><span>IT Software Solution</span></h1>
+      <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.<br/> Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,</p>
+      <button className='quote-btn'>Get Quote</button>
+      </div>
+      <div className='right-side'>
+        <img src="../../../../hand.png" alt="hand" width="58" height="58" />
+        <p>You deserve the best.<br/>We specialize in delivering tailored<br/> software solutions and DevOps<br/> outsourcing services.</p>
+        <div className='stats'>
+          <div className='stat-item'>
+            <div className='item'>
+              <img src="../../../../user.png" alt="user" />
+              02
+            </div>
+            <p>Year Experience</p>
+          </div>
+          <div className='line'></div>
+          <div className='stat-item'>
+          <div className='item'>
+          <img src="../../../../briefcase.png" alt="briefcase" />
+          50 +
+          </div>
+          <p>Project Complete</p>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
