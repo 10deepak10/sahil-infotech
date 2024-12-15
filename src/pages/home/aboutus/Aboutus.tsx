47b6f13e6f92e9ad675840c11253821b98aae0d1
deepak <mohantydeepak309@gmail.com>
2024-12-15 21:53:55 +0530

    textual change

--- src/pages/home/aboutus/Aboutus.tsx
+++ src/pages/home/aboutus/Aboutus.tsx
@@ -55,7 +55,7 @@ export default function Aboutus() {
       </div>
       <div className="right-side">
         <h2>Who We Are</h2>
-        <p className="sub-title">
+        <p className="text">
           Empowering businesses with innovative digital solutions that build
           meaningful connections and drive success.
         </p>
